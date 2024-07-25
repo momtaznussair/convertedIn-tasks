4671def303bdb25f9f6489e03607e92639ceea19
Momtaz Nussair <m.nussair@silkysys.com>
2024-07-25 19:32:39 +0300

    Create tasks route group

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Livewire\Tasks\CreateTask;
+use App\Livewire\Tasks\Tasks;
+use App\Livewire\Tasks\TasksStatistics;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::redirect('/', '/tasks');
+
+Route::prefix('tasks')
+->name('tasks.')
+->group(function () {
+    Route::get('/', Tasks::class)->name('list');
+    Route::get('/create', CreateTask::class)->name('create');
+    Route::get('/statistics', TasksStatistics::class)->name('statistics');
 });
+
